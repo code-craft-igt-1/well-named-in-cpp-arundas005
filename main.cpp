1e32627dedf78c99eb9fd2adb0d9dcb8746655b5
Arundas <51848726+arundas005@users.noreply.github.com>
2024-08-01 16:38:32 +0530

    Added test for print functionality

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include "TestColorCoding.h"
-#include "ColorCodePrinter.h"
+#include "TestRefManualPrinting.h"
 
-int main() 
-{
+int main() {
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
     testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
 
     testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
 
-    TelCoColorCoder::PrintColorCodeRefManual();
+    testPrintColorCodeRefManual();
+
     return 0;
 }
